c69e046f73601c07a1122d0672026cb618d11630
faalbers <chalbers@gmail.com>
2021-07-16 15:42:53 -0700

    fixed async

--- async.cpp
+++ async.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <thread>
 #include <future>
 
 typedef unsigned long int ull;
 
-#define ASYNC 0
+#define ASYNC 1
+
+std::mutex m;
 
 ull findOddSum(ull start, ull end)
 {
+    m.lock();
+    std::cout << "findOddSum ID: " << std::this_thread::get_id() << std::endl;
+    m.unlock();
+
     ull oddSum = 0;
     for(ull x = start; x <= end; x++) {
         if(x & 1) oddSum += x;
@@ -21,10 +28,24 @@ int main()
 
 #if ASYNC
     std:: cout << "Run task async" << std::endl;
+    std::future<ull> OddSum = std::async(std::launch::async, findOddSum, start, end);
 #else
     std:: cout << "Run task deffered" << std::endl;
     std::future<ull> OddSum = std::async(std::launch::deferred, findOddSum, start, end);
 #endif
+    m.lock();
+    std::cout << "main       ID: " << std::this_thread::get_id() << std::endl;
+    m.unlock();
+
+    std::cout << "start pause ...\n";
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    std::cout << "end pause ...\n";
+
+    ull oddSum = OddSum.get();
+
+    std::cout << "Result: " << oddSum << std::endl;
+
+    std::cout << "Completed !\n";
     
     return 0;
 }
